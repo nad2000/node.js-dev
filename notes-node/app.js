9485ada61b4cb9634cec580baebf4b3044ed3bf8
Rad Cirskis <nad2000@gmail.com>
2017-12-30 19:48:06 +1300

    added func.

--- notes-node/app.js
+++ notes-node/app.js
@@ -6,12 +6,15 @@ const notes = require("./notes.js");
 
 var user = os.userInfo();
 // fs.appendFile("greetings.txt", "Hello world!\n");  // depricated
-fs.appendFile("greetings.txt", `Hello ${user.username}!, You are ${notes.age}.\n`, function (err) {
-  if (err) {
-    console.log("Unable to write to file.");
-  }
-});
+// fs.appendFile("greetings.txt", `Hello ${user.username}!, You are ${notes.age}.\n`, function (err) {
+//   if (err) {
+//     console.log("Unable to write to file.");
+//   }
+// });
 
 // OR:
 //fs.appendFileSync("greetings.txt", "Hello world!\n");
 
+
+var res = notes.addNote();
+console.log(res);
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -3,4 +3,10 @@ console.log("Starting notes.js");
 //console.log(module);
 module.exports.age = 25;
 
+// arrow function doesn't bind 'this' and ...
+module.exports.addNote = () => { // loosly equivalent to anonymous func.: function() {
+  console.log("addNote");
+  return "New Note";
+};
+
 
