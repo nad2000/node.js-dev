d38825f338fed8f5e2589ba2204cf944e22b38ab
Rad Cirskis <nad2000@gmail.com>
2017-12-30 19:35:50 +1300

    greeting user...

--- notes-node/app.js
+++ notes-node/app.js
@@ -3,16 +3,14 @@ console.log("Starting app.");
 const fs = require("fs");
 const os = require("os");
 
-// fs.appendFile("greetings.txt", "Hellow world!\n");  // depricated
-fs.appendFile("greetings.txt", "Hellow world!\n", function (err) {
+var user = os.userInfo();
+// fs.appendFile("greetings.txt", "Hello world!\n");  // depricated
+fs.appendFile("greetings.txt", "Hello " + user.username + "!\n", function (err) {
   if (err) {
     console.log("Unable to write to file.");
   }
 });
 
 // OR:
-fs.appendFileSync("greetings.txt", "Hellow world!\n");
+//fs.appendFileSync("greetings.txt", "Hello world!\n");
 
-
-var user = os.userInfo();
-console.log(user);
