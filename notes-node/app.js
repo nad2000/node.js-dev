182465c63845a875ace73e8379fc3f6dad3dd319
Rad Cirskis <nad2000@gmail.com>
2017-12-30 19:34:06 +1300

    print user info...

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,8 +1,9 @@
 console.log("Starting app.");
 
 const fs = require("fs");
+const os = require("os");
 
-fs.appendFile("greetings.txt", "Hellow world!\n");  // depricated
+// fs.appendFile("greetings.txt", "Hellow world!\n");  // depricated
 fs.appendFile("greetings.txt", "Hellow world!\n", function (err) {
   if (err) {
     console.log("Unable to write to file.");
@@ -11,3 +12,7 @@ fs.appendFile("greetings.txt", "Hellow world!\n", function (err) {
 
 // OR:
 fs.appendFileSync("greetings.txt", "Hellow world!\n");
+
+
+var user = os.userInfo();
+console.log(user);
