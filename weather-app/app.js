ac16f237913e4bea7c7c894784e90f39187cb1c2
Rad Cirskis <nad2000@gmail.com>
2018-01-05 17:16:29 +1300

    better formattting...

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,5 +4,8 @@ request({
   url: "https://maps.googleapis.com/maps/api/geocode/json?address=85%20symonds%20street%20grafton%20auckland%20new%20zealand",
   json: true
 }, (error, response, body) => {
-  console.log(body);
+  console.log(JSON.stringify(body, undefined, 2));
+  //console.log(JSON.stringify(body, (k, v) => {
+  // if (k !== "place_id") return v;
+  // }, 2));
 });
