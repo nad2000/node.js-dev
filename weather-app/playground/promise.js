6915b6a62acba2c04dbf59bf110d7ef0bf1edad9
Rad Cirskis <nad2000@gmail.com>
2018-01-10 19:47:50 +1300

    more examples...

--- weather-app/playground/promise.js
+++ weather-app/playground/promise.js
@@ -1,15 +1,39 @@
-var somePromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("Hey. It  worked!");
-    resolve(); // thist one won't get called as with callbacks
-    reject("Unable to fulfill promise");
-  }, 2500);
-});
+var asyncAdd = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (typeof a === "number" && typeof b === "number") {
+        resolve(a + b);
+      } else {
+        reject("Arguments must be numbers.");
+      }
+    }, 2500);
+  });
+};
 
-
-somePromise.then((message) => {
-  console.log("Success:", message);
+asyncAdd(5, 7).then((result) => {
+  console.log("Success:", result);
 }, (message) => {
   console.log("Error:", message);
 });
+
+asyncAdd(5, undefined).then((result) => {
+  console.log("Success:", result);
+}, (message) => {
+  console.log("Error:", message);
+});
+
+// var somePromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("Hey. It  worked!");
+//     resolve(); // thist one won't get called as with callbacks
+//     reject("Unable to fulfill promise");
+//   }, 2500);
+// });
+
+
+// somePromise.then((message) => {
+//   console.log("Success:", message);
+// }, (message) => {
+//   console.log("Error:", message);
+// });
 
