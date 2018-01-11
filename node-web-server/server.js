9774bd8b8663ac8f0e39f92c66e12174ced97133
Rad Cirskis <nad2000@gmail.com>
2018-01-11 21:51:49 +1300

    more routes...

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -3,7 +3,16 @@ const express = require("express");
 var app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello Express!");
+  // res.send("<h1>Hello Express!</h1>");
+  res.send({name: "Rad", likes: ["Cats", "Dogs", ]});
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1>About Page</h1>");
+});
+
+app.get("/bad", (req, res) => {
+  res.send({errorMessage: "Failed to execute the request."});
 });
 
 app.listen(3333);
