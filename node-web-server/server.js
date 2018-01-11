ae944ef6207e4e271d29bc7919f5b90151f1e7e4
Rad Cirskis <nad2000@gmail.com>
2018-01-11 22:16:04 +1300

    added templates and view engine...

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,16 +1,28 @@
 const express = require("express");
+const hbs = require("hbs");
 
 var app = express();
 
+app.set("view engine", "hbs"); // this is optional
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
   // res.send("<h1>Hello Express!</h1>");
-  res.send({name: "Rad", likes: ["Cats", "Dogs", ]});
+  // res.send({name: "Rad", likes: ["Cats", "Dogs", ]});
+  res.render("home.hbs",
+    {
+      pageTitle: "Home Page",
+      currentYear: new Date().getFullYear()
+    });
 });
 
 app.get("/about", (req, res) => {
-  res.send("<h1>About Page</h1>");
+  //res.send("<h1>About Page</h1>");
+  res.render("about.hbs",
+    {
+      pageTitle: "About Page",
+      currentYear: new Date().getFullYear()
+    });
 });
 
 app.get("/bad", (req, res) => {
