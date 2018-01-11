5430fcdc2a3e9b851546e8f9c559c7a6117b0159
Rad Cirskis <nad2000@gmail.com>
2018-01-11 21:59:22 +1300

    added static conten serving...

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 var app = express();
 
+app.use(express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => {
   // res.send("<h1>Hello Express!</h1>");
   res.send({name: "Rad", likes: ["Cats", "Dogs", ]});
@@ -15,4 +17,6 @@ app.get("/bad", (req, res) => {
   res.send({errorMessage: "Failed to execute the request."});
 });
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log("Server is up on port 3333");
+});
