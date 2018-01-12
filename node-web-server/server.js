9c33942d5a455a7bc0aeb12c68d5ea939d864ede
Rad Cirskis <nad2000@gmail.com>
2018-01-12 19:44:30 +1300

    improved logging middleware into a file

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const hbs = require("hbs");
+const fs = require("fs");
 
 var app = express();
 
@@ -13,32 +14,39 @@ hbs.registerHelper("screamIt", text => text.toUpperCase());
 // Logging middleware:
 app.use((req, res, next) => {
   var now = new Date().toISOString();
-  console.log(`${now}: ${req.method} ${req.url}`);
+  var msg = `${now}: ${req.method} ${req.url}`
+  console.log(msg);
+  fs.appendFile("server.log", msg + '\n', (err) => {
+    if (err) {
+      console.log("Unable to append to server.log.");
+    }
+  });
   next();
 });
 
 app.get("/", (req, res) => {
   // res.send("<h1>Hello Express!</h1>");
   // res.send({name: "Rad", likes: ["Cats", "Dogs", ]});
-  res.render("home.hbs",
-    {
-      pageTitle: "Home Page",
-      welcomeMessage: "Welcome to my website"
-    });
+  res.render("home.hbs", {
+    pageTitle: "Home Page",
+    welcomeMessage: "Welcome to my website"
+  });
 });
 
 app.get("/about", (req, res) => {
   //res.send("<h1>About Page</h1>");
-  res.render("about.hbs",
-    {
-      pageTitle: "About Page"
-    });
+  res.render("about.hbs", {
+    pageTitle: "About Page"
+  });
 });
 
 app.get("/bad", (req, res) => {
-  res.send({errorMessage: "Failed to execute the request."});
+  res.send({
+    errorMessage: "Failed to execute the request."
+  });
 });
 
 app.listen(3333, () => {
   console.log("Server is up on port 3333");
 });
+
