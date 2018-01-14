fef74d253aa7bc70c850e8155201749fcea99b3b
Rad Cirskis <nad2000@gmail.com>
2018-01-14 21:12:52 +1300

    more test examples...

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -3,11 +3,17 @@ const expect = require("expect");
 it(
   "should add two numbers", () => {
     var res = utils.add(33, 11);
-    expect(res).toBe(44);
+    expect(res).toBe(44).toBeA("number");
   });
 
 it(
   "should shquare a number", () => {
     var res = utils.square(3);
-    expect(res).toBe(9);
+    expect(res).toBe(9).toBeA("number");
+  });
+
+
+it(
+  "shoud expect some value", () => {
+    expect(12).toNotBe(11);
   });
