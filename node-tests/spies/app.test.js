2b8edcde4f0450c105bfdd9286e94c46d7cf6348
Rad Cirskis <nad2000@gmail.com>
2018-01-16 19:19:42 +1300

    added test for underlying api

--- node-tests/spies/app.test.js
+++ node-tests/spies/app.test.js
@@ -1,11 +1,30 @@
 const expect = require("expect");
+const rewire = require("rewire");
+
+var app = rewire("./app"); // user 'rewire' instead of 'require' to mock out services...
 
 describe("Application", () => {
+  var db = {
+    saveUser: expect.createSpy()
+  };
+  app.__set__("db", db);
+
+  it("should call saveUser with user object", () => {
+    var email = "test123@test.com";
+    var password = "p455w0rd";
+
+    app.handleSignup(email, password);
+    expect(db.saveUser).toHaveBeenCalledWith({
+      email,
+      password
+    });
+  });
 
   it("Shoud call the spy correctly", () => {
     var spy = expect.createSpy();
     spy("Rad", 42);
     expect(spy).toHaveBeenCalledWith("Rad", 42);
   });
+
 });
 
