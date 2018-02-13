202e34b443c86026478f35f0651128be99d8dd12
Rad Cirskis <nad2000@gmail.com>
2018-02-13 19:55:21 +1300

    basic features...

--- async-await/app-promises.js
+++ async-await/app-promises.js
@@ -44,9 +44,6 @@ const getUser = id => {
 
 
 const getStatus = id => {
-  // return new Promise((resolve, reject) => {
-
-  // });
   let user;
   return getUser(id).then(_user => {
     user = _user;
@@ -59,7 +56,37 @@ const getStatus = id => {
   });
 }
 
-getStatus(1).then(status => console.log(status)).catch(e => console.log("ERROR:", e));
+const getMike = async id => {
+  return "Mike";
+}
+
+// is identical to:
+const getMikeOld = id => {
+  return new Promise((resolve, reject) => {
+    resolve("Mike");
+  });
+};
+
+// Alt with async/await
+const getStatusAlt = async id => {
+  const user = await getUser(id);
+  const grades = await getGrades(user.schoolId);
+  let avg = 0.0;
+  if (grades && grades.length > 0)
+    avg = grades.map(g => g.grade).reduce((a, b) => a + b) / grades.length;
+  return `${user.name} has a ${avg}% in the class.`;
+}
+
+// Alt with async/await (reject using an exception)
+const getStatusAlt2 = async id => {
+    throw new Error("This is an error.");
+    return "Mike";
+  }
+  // getStatus(1).then(status => console.log(status)).catch(e => console.log("ERROR:", e));
+  // console.log(getStatusAlt());
+getStatusAlt(22).then(status => console.log(status)).catch(e => console.err("ERROR:", err));
+// getStatusAlt2().then(name => console.log(name)).catch(e => console.err("ERROR:", err));
+
 
 // getGrades(101).then(grades => console.log(grades));
 // getGrades(999).then(grades => console.log(grades));
